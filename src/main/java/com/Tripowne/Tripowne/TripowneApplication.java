c1a4558d0a9f71b8c86a031e4c46f39ff85e05ee
JIYE KIM <hyo37009@gmail.com>
2025-03-23 14:01:25 +0900

    init

---
+++ src/main/java/com/Tripowne/Tripowne/TripowneApplication.java
@@ -0,0 +1,13 @@
+package com.Tripowne.Tripowne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripowneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripowneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tripowne/Tripowne/TripowneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tripowne.Tripowne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripowneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
