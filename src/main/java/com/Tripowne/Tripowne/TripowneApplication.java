bcd0dcca30c451a822df0fad340a25d64e145ac0
JIYE KIM <hyo37009@gmail.com>
2025-03-27 18:54:12 +0900

    :tada:

--- src/main/java/com/Tripowne/Tripowne/TripowneApplication.java
+++ src/main/java/com/Tripowne/Tripowne/TripowneApplication.java
@@ -2,8 +2,11 @@ package com.Tripowne.Tripowne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+// DB 연결 없이 사용하기 위해 (exclude={DataSourceAutoConfiguration.class}) 추가
+@SpringBootApplication()
 public class TripowneApplication {
 
 	public static void main(String[] args) {
